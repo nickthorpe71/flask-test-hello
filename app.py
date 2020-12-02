b6d94edaab97feb2d64ccffabda02012c53db9a9
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-02 08:55:14 -0600

    adds templates folder and index.html

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 # SETUP: https://realpython.com/flask-by-example-part-1-project-setup/
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
+
+@app.route('/bye')
+def bye():
+    return "Googbye"
 
 if __name__ == '__main__':
     app.run()
